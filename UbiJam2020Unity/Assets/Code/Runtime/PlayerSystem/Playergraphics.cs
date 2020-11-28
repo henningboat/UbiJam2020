95a2b07669a8b953020f908553dee56c58725754
henningboat <34309099+henningboat@users.noreply.github.com>
2020-11-28 15:11:07 +0100

    Set up 2D Rig for Player.prefab

---
+++ UbiJam2020Unity/Assets/Code/Runtime/PlayerSystem/Playergraphics.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Playergraphics : MonoBehaviour
+{
+	#region Serialize Fields
+
+	private List<Transform> bodyParts;
+	[SerializeField,] private float rotationSpeed = 100;
+	[SerializeField,] private float minDistance = 1;
+
+	[SerializeField] private Transform _startBone;
+
+
+	#endregion
+
+	#region Private Fields
+
+	private Vector2 lastFramePosition;
+
+	#endregion
+
+	#region Unity methods
+
+	void Start()
+	{
+		lastFramePosition = transform.position;
+		bodyParts = _startBone.transform.GetComponentsInChildren<Transform>().ToList();
+		for (int i = 1; i < bodyParts.Count; i++)
+		{
+			bodyParts[i].SetParent(null,true);
+		}
+	}
+
+	void Update()
+	{
+		float curspeed = ((Vector2) transform.position - lastFramePosition).magnitude;
+
+		for (int i = 1; i < bodyParts.Count; i++)
+		{
+			var curBodyPart = bodyParts[i];
+			var PrevBodyPart = bodyParts[i - 1];
+
+			var dis = Vector3.Distance(PrevBodyPart.position, curBodyPart.position);
+
+			Vector3 newpos = PrevBodyPart.position;
+
+			float T = ((Time.deltaTime * dis) / minDistance) * curspeed;
+
+			if (T > 0.5f)
+			{
+				T = 0.5f;
+			}
+
+			curBodyPart.position = Vector3.Slerp(curBodyPart.position, newpos, T);
+			curBodyPart.rotation = Quaternion.Slerp(curBodyPart.rotation, PrevBodyPart.rotation, T);
+		}
+
+		lastFramePosition = transform.position;
+	}
+
+	#endregion
+}
