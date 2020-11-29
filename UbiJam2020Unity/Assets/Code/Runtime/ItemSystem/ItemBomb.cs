958a1b8de8ffb049e1c7f8f58a9f9254941bb2f5
henningboat <34309099+henningboat@users.noreply.github.com>
2020-11-29 16:43:30 +0100

    Updated item graphics

--- UbiJam2020Unity/Assets/Code/Runtime/ItemSystem/ItemBomb.cs
+++ UbiJam2020Unity/Assets/Code/Runtime/ItemSystem/ItemBomb.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Runtime.ItemSystem
@@ -12,6 +13,8 @@ namespace Runtime.ItemSystem
 		[SerializeField,] private float _radius;
 		[SerializeField,] private SpriteRenderer _bombSprite;
 		[SerializeField,] private SpriteRenderer _explosionSprite;
+		[SerializeField,] private SpriteRenderer _explosionSprite2;
+		[SerializeField,] private SpriteRenderer _smokeSprite;
 		[SerializeField] private AudioSource _audioClip;
 
 		#endregion
@@ -24,12 +27,19 @@ namespace Runtime.ItemSystem
 			_audioClip.Play();
 			yield return null;
 			yield return null;
-			yield return null;
 			_bombSprite.enabled = false;
 			_explosionSprite.enabled = true;
-			yield return null;
+			yield return new WaitForFixedUpdate();
 			_explosionSprite.enabled = false;
+			_explosionSprite2.enabled = true;
+			
 			GameSurface.GameSurface.Instance.DestroyCircle(transform.position, _radius);
+		
+			yield return new WaitForFixedUpdate();
+			_explosionSprite2.enabled = false;
+			_smokeSprite.enabled = true;
+			_smokeSprite.transform.DOScale(_smokeSprite.transform.localScale.x * 2f, 3f);
+			_smokeSprite.DOFade(0, 3f);
 		}
 
 		#endregion
